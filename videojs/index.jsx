7a6b9b158dd1a4baf89c3078054da80d65877dd6
xiasong <xias@uchonor.com>
2021-01-27 08:37:54 +0800

    demo

---
+++ videojs/index.jsx
@@ -0,0 +1,14 @@
+const {
+  useEffect,
+  useState,
+  useRef,
+  useContext,
+  createContext,
+  Component,
+} = React;
+
+const App = () => {
+  return <div>111</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
